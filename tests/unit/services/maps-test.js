bfa0c465360f42333674f9ed2dcee13fae86dd3c
Brian Tenggren <btenggren20@gmail.com>
2017-02-17 14:56:32 -0500

    maps n stuff

--- tests/unit/services/maps-test.js
+++ tests/unit/services/maps-test.js
@@ -1,12 +1,39 @@
 import { moduleFor, test } from 'ember-qunit';
+import Ember from 'ember';
 
-moduleFor('service:maps', 'Unit | Service | maps', {
-  // Specify the other units that are required for this test.
-  // needs: ['service:foo']
+const DUMMY_ELEMENT = {};
+
+let MapUtilStub = Ember.Object.extend({
+  createMap(element, location) {
+    this.assert.ok(element, 'created map with element');
+    this.assert.ok(location, 'created map with location');
+  }
 });
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  let service = this.subject();
-  assert.ok(service);
+moduleFor('service:maps', 'Unit | Service | maps', {
+  // We need to require our google maps utility to use the maps service but we stubbed out the util above
+  needs: ['util:google-maps']
+});
+
+test('should create a new map id one isnt cached for location', function(assert) {
+  // We expect there to be four maps? maybe?
+  assert.expect(4);
+
+  let stubMapUtil = MapUtilStub.create({ assert });
+  let mapService = this.subject({ mapUtil: stubMapUtil });
+  let element = mapService.getMapElement('San Francisco');
+
+  assert.ok(element, 'element exists');
+  assert.ok(element.className, 'map', 'element has class name of map');
+});
+
+test('should use an existing map if one is chached for location', function(assert) {
+  assert.expect(1);
+  let stubCachedMaps = Ember.Object.create({
+    sanFrancisco: DUMMY_ELEMENT
+  });
+  let mapService = this.subject({ cachedMaps: stubCachedMaps });
+  let element = mapService.getMapElement('San Francisco');
+
+  assert.equal(element, DUMMY_ELEMENT, 'element fetched from cache');
 });
